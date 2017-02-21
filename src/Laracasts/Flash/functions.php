26ec69394c9c633fe9deec562c706889b590e73c
Mihai-Florin Moinescu <moinescumihai@gmail.com>
2017-02-21 16:49:34 +0200

    Added helper functions for success, info, warning and danger flash messages

--- src/Laracasts/Flash/functions.php
+++ src/Laracasts/Flash/functions.php
@@ -19,5 +19,61 @@ if ( ! function_exists('flash')) {
 
         return $notifier;
     }
+}
 
+if ( ! function_exists('flashInfo')) {
+
+    /**
+     * Arrange for an info flash message.
+     *
+     * @param  string|null $message
+     * @return \Laracasts\Flash\FlashNotifier
+     */
+    function flashInfo($message = null)
+    {
+        return flash($message, 'info');
+    }
+}
+
+
+if ( ! function_exists('flashSuccess')) {
+
+    /**
+     * Arrange for an success flash message.
+     *
+     * @param  string|null $message
+     * @return \Laracasts\Flash\FlashNotifier
+     */
+    function flashSuccess($message = null)
+    {
+        return flash($message, 'success');
+    }
+}
+
+if ( ! function_exists('flashWarning')) {
+
+    /**
+     * Arrange for an success flash message.
+     *
+     * @param  string|null $message
+     * @return \Laracasts\Flash\FlashNotifier
+     */
+    function flashWarning($message = null)
+    {
+        return flash($message, 'warning');
+    }
+}
+
+if ( ! function_exists('flashDanger')) {
+
+    /**
+     * Arrange for an success flash message.
+     *
+     * @param  string|null $message
+     * @return \Laracasts\Flash\FlashNotifier
+     */
+    function flashDanger($message = null)
+    {
+        return flash($message, 'danger');
+    }
 }
